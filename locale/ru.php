47e42b991e098b1fd2110114bd98a1efe821cab0
Jason Barnabe <jason.barnabe@gmail.com>
2014-11-03 21:43:01 -0600

    Russian

---
+++ locale/ru.php
@@ -0,0 +1,4 @@
+<?php
+$Definition['DiscussionAbout.AboutItemSubtitle'] = 'Скрипт: %s';
+$Definition['DiscussionAbout.AboutItemHeader'] = 'скрипт %s';
+$Definition['Item ID:'] = 'Идентификатор элемента:';
